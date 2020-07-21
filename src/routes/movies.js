6eccf52b5871927b5b749109b4f7c0fa0bcd8ec3
Palo23 <lmarespinortizz23@gmail.com>
2020-07-21 14:04:08 -0600

    first api

---
+++ src/routes/movies.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+const router = Router();
+const movies = require('../example.json');
+const _ = require('underscore');
+
+router.get('/', (req, res) => {
+    res.json(movies);
+});
+
+router.post('/', (req, res) => {
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        const id = movies.length + 1;
+        const newMovie = { id, ...req.body };
+        movies.push(newMovie);
+        res.json(movies);
+    } else {
+        res.status(500).json({ error: "There was an error" });
+    }
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(movies, (movie, i) => {
+        if (movie.id == id) {
+            movies.splice(i, 1)
+        }
+    })
+    res.json(movies);
+});
+
+module.exports = router;
